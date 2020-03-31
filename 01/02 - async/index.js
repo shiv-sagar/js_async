1410b478468d2c90247761e821d1aa0e9c36ecec
shivsagar <shivsagar.maykar@gmail.com>
2020-03-31 11:27:34 +0530

    complete exercise 1 and 2 in the repo

--- 01/02 - async/index.js
+++ 01/02 - async/index.js
@@ -9,20 +9,24 @@ function testMe() {
   console.log("Partnah");
 }
 // After thinking it through, uncomment the following line to check your guess!
-// testMe(); // what order should these log out? Howdy or Partnah first?
+testMe(); // what order should these log out? Howdy or Partnah first? First it will log out Partnah and then Howdy.
 
 /* CHALLENGE 2 */
 
 function delayedGreet() {
   // ADD CODE HERE
+  console.log("welcome");
 }
+setTimeout(delayedGreet, 3000);
 // Uncomment the following line to check your work!
-// delayedGreet(); // should log (after 3 seconds): welcome
+ delayedGreet(); // should log (after 3 seconds): welcome
 
 /* CHALLENGE 3 */
 
 function helloGoodbye() {
-  // ADD CODE HERE
+  // ADD CODE HERe
+  setTimeout(() => console.log("good bye"), 3000);
+  console.log("hello");
 }
 // Uncomment the following line to check your work!
 // helloGoodbye(); // should log: hello // should also log (after 3 seconds): good bye
@@ -31,25 +35,41 @@ function helloGoodbye() {
 
 function brokenRecord() {
   // ADD CODE HERE
+  console.log("hi again")
 }
+setInterval(brokenRecord,1000)
 // Uncomment the following line to check your work!
-// brokenRecord(); // should log (every second): hi again
+ brokenRecord(); // should log (every second): hi again
 
 /* CHALLENGE 5 */
 
 function limitedRepeat() {
   // ADD CODE HERE
+  let count = 0;
+  let inter = setInterval(() => {
+    count++;
+    console.log("hi for now");
+    if (count > 4) {
+      clearInterval(inter);
+    }
+  },1000 )
 }
 // Uncomment the following line to check your work!
-// limitedRepeat(); // should log (every second, for 5 seconds): hi for now
+ limitedRepeat(); // should log (every second, for 5 seconds): hi for now
 
 /* CHALLENGE 6 */
 
-function everyXsecsForYsecs() {
+function everyXsecsForYsecs(func, intervalNo, durationNo) {
   // ADD CODE HERE
+  let count = 1;
+  let interval = setInterval(() => {
+    count++;
+    func();
+    if(count > durationNo) clearInterval(interval);
+  }, intervalNo*1000);
 }
 // Uncomment the following lines to check your work!
-// function theEnd() {
-//   console.log('This is the end!');
-// }
-// everyXsecsForYsecs(theEnd, 2, 20); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
+ function theEnd() {
+   console.log('This is the end!');
+ }
+ everyXsecsForYsecs(theEnd, 2, 20); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
--- 02 - XMLHttpRequest/github-user/main.js
+++ 02 - XMLHttpRequest/github-user/main.js
@@ -1,1 +1,60 @@
 // Your code goes here
+let input = document.querySelector('.input');
+let search = document.querySelector('.search_btn');
+let card = document.querySelector('.card');
+
+
+function handleClick(e) {
+    e.preventDefault();
+
+    if(input.value.trim()) {
+        let username = input.value;
+        let url = 'https://api.github.com/users/';
+        fetchInfo(username,url);
+        input.value = '';       
+    }
+}
+
+function fetchInfo(username,url) {
+    var xmlhttp = new XMLHttpRequest();
+    let method = "GET"
+    url = `https://api.github.com/users/${username}`; 
+    xmlhttp.open(method,url,true); // initializes the newly created network request takes true parameter deafult for asynchronous opertion.
+
+
+  // pass the  retrieved data ( found in xmlhttp.response ) to createUI function.
+    xmlhttp.onload = function() {
+
+        if(xmlhttp.status != 404) {
+            userDetail = JSON.parse(xmlhttp.response);
+            createUI(userDetail);
+        } else {
+            card.style.visibility = 'visible';
+            card.classList.add('errormsg');
+            card.innerHTML = `<p style="color: #fff"> User was not found! :( </p>`
+        }
+    }
+
+    xmlhttp.send();
+}
+
+function createUI(object) {
+    console.log(object);
+   
+    card.style.visibility = 'visible';
+   
+    let addDom = `<div class = "imgWrapper">
+                    <img src = "${object.avatar_url}" alt = "profile image">
+                </div>
+
+                <div class = "user_info">
+                    <p>Name : ${object.name || ""}</p>
+                    <p>Id : ${object.id}</p>
+                    <p>Bio : ${object.bio || ''}</p> 
+                </div>`;
+    
+    card.innerHTML = addDom;
+
+}
+
+search.addEventListener('click',handleClick);
